208fbf283b8f9b2481c9ab9cce2f11a743dde18e
mashkuma <152713780+mashkuma@users.noreply.github.com>
2023-12-03 21:46:54 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,6 @@ import { Web3Button } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  //const tokenId = 0;
-  //const quantity = 1;
-  //tokenId, quantity
 
   return (
     <div className={styles.container}>
