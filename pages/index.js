33142790835bbb0f93ae249e47e25121cfc3a59c
mashkuma <mashkuma.kawaii@gmail.com>
2023-12-05 18:05:36 +0900

    CSS 更新

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,42 @@
-import { Web3Button } from "@thirdweb-dev/react";
+import { useConnectionStatus, ConnectWallet, Web3Button } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const connectionStatus = useConnectionStatus();
 
   const handleLogoClick = () => {
-    // ましゅくまの文字をクリックしたときの処理
     window.location.href = "https://mashkuma.studio.site";
   };
 
+  const shouldShowWeb3Button = connectionStatus === "connected";
+
   return (
     <div className={styles.container}>
-      <h1 className={styles.pageTitle}>
-        ましゅくまアイコンをもらおう！
-      </h1>
-      <p className={styles.customParagraph1}>
-        ↓Get it from here!↓
-      </p>
+      <h1 className={styles.pageTitle}>ましゅくまアイコンをもらおう！</h1>
+      <p className={styles.customParagraph1}>↓Get it from here!↓</p>
       <div className={styles.buttonContainer}>
-        <Web3Button
-          contractAddress="0xA87CfC6393651cE7810A8451fab74f4022cEdA4D"
-          action={(contract) => contract.erc721.claim(1)}
-          onSuccess={() => alert("Claimed!")}
-          onError={() => alert("Something went wrong")}
-        >
-          Claim
-        </Web3Button>
+        <div className={styles.walletContainer}>
+          <ConnectWallet />
+        </div>
+
+        {shouldShowWeb3Button && (
+          <div className={styles.buttonWrapper}>
+            <Web3Button
+              contractAddress="0xA87CfC6393651cE7810A8451fab74f4022cEdA4D"
+              action={(contract) => contract.erc721.claim(1)}
+              onSuccess={() => alert("Claimed!")}
+              onError={() => alert("Something went wrong")}
+            >
+              GET
+            </Web3Button>
+          </div>
+        )}
       </div>
+
       <p className={styles.customParagraph2}>
-        メタマスクなどのウォレットが必要です。
+        {connectionStatus !== "connected" && (
+          <>アカウントを作成、またはログインしてください。</>
+        )}
       </p>
       <p className={styles.customParagraph2} onClick={handleLogoClick}>
         ▷back to offical site◁
